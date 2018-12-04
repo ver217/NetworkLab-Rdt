25839d1cc210f53b68acbdebe5061a2d94fa75d8
ver217 <lhx0217@gmail.com>
2018-12-04 13:26:24 +0800

    fix: use index instead of queue

--- src/GBNRdtSender.cpp
+++ src/GBNRdtSender.cpp
@@ -7,7 +7,8 @@ GBNRdtSender::GBNRdtSender(int n, unsigned int seqnum_bits)
     : SEQ_MAX((seqnum_bits > 0 && seqnum_bits <= 16) ? (1 << seqnum_bits) : (1 << 16)),
       N(n),
       base(0),
-      nextSeqnum(0) {
+      nextSeqnum(0),
+      pkts(vector<Packet>(n))  {
 }
 
 GBNRdtSender::~GBNRdtSender() {
@@ -22,14 +23,14 @@ inline bool GBNRdtSender::in_window(int ackNum) {
 }
 
 bool GBNRdtSender::getWaitingState() {
-    return static_cast<int>(pkts.size()) >= N;
+    return nextSeqnum == static_cast<int>((base + N) % SEQ_MAX);
 }
 
 bool GBNRdtSender::send(Message &message) {
     if (getWaitingState())
         return false;
     Packet pkt = make_data_pkt(nextSeqnum, message.data);
-    pkts.push_back(pkt);
+    pkts[nextSeqnum % N] = pkt;
     pUtils->printPacket("发送方发送报文", pkt);
     pns->sendToNetworkLayer(RECEIVER, pkt);
     if (base == nextSeqnum)
@@ -44,9 +45,6 @@ void GBNRdtSender::receive(Packet &ackPkt) {
         base = (ackPkt.acknum  + 1) % SEQ_MAX;
         pUtils->printPacket("发送方正确收到确认", ackPkt);
         pns->stopTimer(SENDER, 0);
-        while (ackPkt.acknum != pkts[0].seqnum)
-            pkts.erase(pkts.begin());
-        pkts.erase(pkts.begin());
         cout << "base 向前移动变为" << base << endl;
         if (base != nextSeqnum)
             pns->startTimer(SENDER, Configuration::TIME_OUT, 0);
@@ -57,9 +55,9 @@ void GBNRdtSender::receive(Packet &ackPkt) {
 void GBNRdtSender::timeoutHandler(int seqNum) {
     cout << "发送方定时器时间到，重发最多前N个报文" << endl;
     pns->stopTimer(SENDER, 0);
-    for (auto pkt : pkts) {
-        pns->sendToNetworkLayer(RECEIVER, pkt);
-        pUtils->printPacket("发之前的报文", pkt);
+    for (int i = base; i != nextSeqnum; i = (i + 1) % SEQ_MAX) {
+        pns->sendToNetworkLayer(RECEIVER, pkts[i % N]);
+        pUtils->printPacket("发之前的报文", pkts[i % N]);
     }
     pns->startTimer(SENDER, Configuration::TIME_OUT, 0);
 }
